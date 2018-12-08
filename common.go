1fc20875ea7a6399adeb19eb337d076d66350e32
Dustin M <Dustin@ms.pi.lan (not a real email)>
2018-12-08 03:17:39 -0700

    forgot to change a thing due to local/self made library shenanigans

--- common.go
+++ common.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"mine/dlogger"
+	"./dlogger"
 	"time"
 )
 
--- corecmds.go
+++ corecmds.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/andersfylling/disgord"
-	"mine/dlogger"
+	"./dlogger"
 	"strings"
 )
 
